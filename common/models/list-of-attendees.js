50da5bda31c94f38b7bcae0853bc18202b276f59
dikyarga <dikyarga.id@gmail.com>
2018-03-30 19:49:23 +0700

    emit websocket existing attendees rigth after it's created

--- common/models/list-of-attendees.js
+++ common/models/list-of-attendees.js
@@ -1,5 +1,31 @@
 'use strict';
 
 module.exports = function(Listofattendees) {
+  Listofattendees.observe('after save', function(ctx, next) {
+    if (ctx.instance) {
+      const app = Listofattendees.app;
+      const Participant = app.models.Participant;
+      const Event = app.models.Event;
 
+      // get user info
+      Participant.findById(ctx.instance.participantId).then(participant => {
+
+        if (participant) {
+          const userJoined = new Date(participant.createdAt);
+          const now = new Date();
+          // get event info
+
+          Event.findById(ctx.instance.eventId).then(anEvent => {
+            const eventStart = new Date(anEvent.startDateTime);
+            if (userJoined.getTime() < eventStart.getTime()) {
+              app.io.emit('joined-participant', participant);
+            } else {
+              app.io.emit('new-participant', participant);
+            }
+          }) 
+        }
+      });
+    }
+    next();
+  });
 };
--- common/models/participant.js
+++ common/models/participant.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 module.exports = function(Participant) {
   Participant.observe('after save', function(ctx, next) {
     const app = Participant.app;
