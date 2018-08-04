5bc4854848f189f2ad88689756ca3af8c0e88efc
dikyarga <dikyarga.id@gmail.com>
2018-08-05 00:08:58 +0700

    create operation hook to generate slug from title combine with random id first

--- common/models/event.js
+++ common/models/event.js
@@ -1,5 +1,13 @@
 'use strict';
+const slug = require('slug');
+var generate = require('nanoid/generate')
 
 module.exports = function(Event) {
-
+  Event.observe('before save', (ctx, next) => {
+    if (ctx.instance) {
+      const randomId = generate('abcdefghijklmnopqrstuvwxyz', 3);
+      ctx.instance.slug = slug(`${ctx.instance.title}-${randomId}`);
+    }
+    next();
+  });
 };
