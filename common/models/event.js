b28da12a7b7f163cbdfae9942fa7daf602ff11dd
dikyarga <dikyarga.id@gmail.com>
2018-10-06 01:30:23 +0700

    create new endpoint for the report of participant of an event

--- common/models/event.js
+++ common/models/event.js
@@ -2,6 +2,9 @@
 const slug = require('slug');
 var generate = require('nanoid/generate')
 
+const sortByNewest = function(a, b) { return a < b }
+
+
 module.exports = function(Event) {
   Event.observe('before save', (ctx, next) => {
     if (ctx.instance) {
@@ -9,5 +12,53 @@ module.exports = function(Event) {
       ctx.instance.slug = slug(`${ctx.instance.title}-${randomId}`);
     }
     next();
+  });
+
+  Event.report = function (eventId, cb) {
+    Event.findById(eventId, {
+      include: ['participants'],
+    }).then(event => {
+      const eventObj = JSON.parse(JSON.stringify(event))
+      const participants = eventObj.participants;
+      const developer = participants.filter(participant => participant.isDeveloper)
+      const eventStartDateTime = new Date(eventObj.startDateTime)
+      const newParticipants = participants.filter(participant => {
+        const participantJoined = new Date(participant.createdAt)
+        return sortByNewest(eventStartDateTime.getTime(), participantJoined.getTime())
+      })
+      cb(null, participants.length, developer.length, newParticipants.length, participants.length - newParticipants.length)
+    })
+  }
+
+  Event.remoteMethod('report', {
+    http: {
+      path: '/report',
+      verb: 'GET',
+    },
+    accepts: [
+      {
+        arg: 'eventId',
+        type: 'string',
+      }
+    ],
+    returns: [
+      {
+        arg: 'total',
+        type: 'number',
+      },
+      {
+        arg: 'developer',
+        type: 'number',
+      },
+      {
+        arg: 'new',
+        type: 'number',
+      },
+      {
+        arg: 'old',
+        type: 'number,'
+      },
+    ],
+    description: 'participants report of an event',
   });
 };
