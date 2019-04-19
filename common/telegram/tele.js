8f5e99576dc463ffeba7ade4ef4b2e21f399a7bc
dikyarga <dikyarga.id@gmail.com>
2019-04-19 16:28:01 +0700

    move Telegram token and chat id to ENV variable

--- common/telegram/tele.js
+++ common/telegram/tele.js
@@ -1,13 +1,16 @@
-const TelegramBot = require('node-telegram-bot-api');
+'use strict';
 
-const token = '782076025:AAF_oyQlg-dbnBbHtUHDK1JPlG4m4sKVVN0'; // need token bot chat
-const chatId = "336960359" // chat id group telegram
+const TelegramBot = require('node-telegram-bot-api');
+const {TELEGRAM_BOT_API_TOKEN, TELEGRAM_BOT_CHAT_ID} = process.env;
+
+const token = TELEGRAM_BOT_API_TOKEN;
+const chatId = TELEGRAM_BOT_CHAT_ID;
 
 const bot = new TelegramBot(token, {polling: true});
 
 const teleBot = (cMsg) => {
-    console.log('send msg to telegram ...');
-    bot.sendMessage(chatId, cMsg);
-}
+  console.log('send msg to telegram ...');
+  bot.sendMessage(chatId, cMsg);
+};
 
-module.exports = {teleBot : teleBot};
+module.exports = {teleBot: teleBot};
